7fa628fc450e5d4b3acde0752e06d9031b34166c
GiteruGerald <gerryjerry36@gmail.com>
2026-07-13 15:32:58 +0300

    Refactor API calls from utils folder and isolate from Web test logic

---
+++ tests/utils/APIUtils.js
@@ -0,0 +1,40 @@
+class APIUtils
+{
+    constructor(apiContext, loginPayload)
+    {
+        this.apiContext = apiContext
+        this.loginPayload = loginPayload
+    }
+
+    async getAuthToken(){
+        const loginResponse = await this.apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login',
+        {data:this.loginPayload}
+    )
+    const loginResponseJson = await loginResponse.json() 
+    let loginToken = loginResponseJson.token 
+    console.log('Login Token: '+loginToken)
+    return loginToken
+    }
+
+    async createOrder(orderPayload){
+         // Create Order API
+            let response = {}
+            response.token = await this.getAuthToken()
+
+            const orderResponse = await this.apiContext.post('https://rahulshettyacademy.com/api/ecom/order/create-order',{
+                data:orderPayload,
+                headers:{
+                    'Authorization': response.token,
+                    'Content-Type': 'application/json'
+                }
+            })
+            const orderResponseJson = await orderResponse.json()
+            console.log(orderResponseJson)
+            const orderId = orderResponseJson.orders[0]
+            response.orderId = orderId
+            return response
+        
+        }
+
+}
+module.exports = {APIUtils}
